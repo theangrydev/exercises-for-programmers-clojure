e6c9df888a998644ac80fe9cbc610819bad52f86
Liam Williams <liam.williams@zoho.com>
2016-01-15 21:42:39 +0000

    Exercise 1 complete

--- src/exercises_for_programmers_clojure/ex01_saying_hello.clj
+++ src/exercises_for_programmers_clojure/ex01_saying_hello.clj
@@ -1,4 +1,5 @@
 (ns exercises-for-programmers-clojure.ex01_saying_hello)
 
-(defn greeting [person]
-  (printf "Hello, %s, nice to meet you!\n" person))
+(defn -main []
+  (print "What is your name? ")
+  (printf "Hello, %s, nice to meet you!\n" (read-line)))
--- test/exercises_for_programmers_clojure/ex01_saying_hello_test.clj
+++ test/exercises_for_programmers_clojure/ex01_saying_hello_test.clj
@@ -3,6 +3,10 @@
     [exercises-for-programmers-clojure.ex01_saying_hello :refer :all]
     [midje.sweet :refer :all]))
 
-(fact "greeting greets the user"
-      (greeting "Liam") => [""]
-      )
+(fact
+  "greeting greets the user"
+  (-main) => nil
+  (provided
+    (print "What is your name? ") => nil :times 1
+    (read-line) => "Liam"
+    (print "Hello, Liam, nice to meet you!\n") => nil :times 1))
