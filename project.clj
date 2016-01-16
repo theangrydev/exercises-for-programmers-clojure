d4756ab3e7749017ff63d7c543a643b415ead041
Liam Williams <liam.williams@zoho.com>
2016-01-16 15:37:43 +0000

    Use midje 1.8.3

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {
              :dev {
-                   :dependencies [[midje "1.7.0"]]
+                   :dependencies [[midje "1.8.3"]]
                    :plugins      [[lein-midje "3.2"]]}})
